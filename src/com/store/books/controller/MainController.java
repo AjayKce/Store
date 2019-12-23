30b29bcf9f569c764e26ee33fe4e4c57770c4a22
Ajay V <ajay-pt3123@csez.zohocorpin.com>
2019-12-23 11:14:58 +0530

    added

--- src/com/store/books/controller/MainController.java
+++ src/com/store/books/controller/MainController.java
@@ -3,11 +3,9 @@ package com.store.books.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.store.books.utilities.StoreUtil;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class MainController {
